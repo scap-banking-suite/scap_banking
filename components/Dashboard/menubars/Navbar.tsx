e82fd4754ca4a4e17935f7a0b8631ec76c255a60
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2024-08-28 12:45:19 +0100

    feat: navbar setup

--- components/Dashboard/menubars/Navbar.tsx
+++ components/Dashboard/menubars/Navbar.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
 
-type Props = {}
+import SearchInputComp from "@/components/controlInput/SearchInputComp";
+import { CollapseActiveIcon, FavouriteIcon, LightModeIcon, NotificationIcon } from "@/icons/svgComp/NavbarIcons";
+import React from "react";
+
+type Props = {};
 
 export const Navbar = (props: Props) => {
   return (
-    <div className='bg-white border-b border-b-borderblack h-[68px] py-4 px-7'>Navbar</div>
-  )
-}
-
-export default Navbar
+    <nav className="bg-white border-b border-b-borderblack h-[68px] flex items-center justify-between py-4 px-7">
+      {/* <p>bjv</p> */}
+      <div className="flex gap-5 items-center">
+        <CollapseActiveIcon />
+        <FavouriteIcon />
+        <div className="flex items-center gap-4">
+          <p className="text-sm text-[#1C1C1C66] font-normal">Dashboards</p>
+          <span className="text-[#1C1C1C33] text-sm font-normal">/</span>
+          <p className="text-[#1C1C1C] text-sm font-normal">Overview</p>
+        </div>
+      </div>
+      <div className="flex gap-5 items-center">
+        <SearchInputComp placeholder="Search" className="" />
+        <LightModeIcon />
+        <NotificationIcon />
+        <CollapseActiveIcon />
+      </div>
+    </nav>
+  );
+};
