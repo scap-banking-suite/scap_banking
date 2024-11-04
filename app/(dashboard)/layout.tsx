015d6f9d362b1d4b94bcac0b6f960e143e53a352
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2024-11-04 18:50:45 +0100

    feat: update to ledger list and sidebar state

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,14 +3,35 @@ import { Navbar } from "@/components/Dashboard/menubars/Navbar";
 import { Rightbar } from "@/components/Dashboard/menubars/Rightbar";
 import { Sidebar } from "@/components/Dashboard/menubars/Sidebar";
 import notificationStore from "@/store/notificationStore";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-  const { notificationVisible, sideBarVisible } = notificationStore();
+  const { notificationVisible, sideBarVisible, setSideBarVisible } = notificationStore();
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1024) {
+        setSideBarVisible(true);
+      } else {
+        setSideBarVisible(false);
+      }
+    };
+
+    // Initial check
+    handleResize();
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className="flex bg-[url('/backgrounddash.png')] bg-no-repeat bg-contain bg-bottom ">
--- components/Account/GeneralLedger/LedgerList/LedgerListTable.tsx
+++ components/Account/GeneralLedger/LedgerList/LedgerListTable.tsx
@@ -21,35 +21,41 @@ const headers = [
 ];
 
 // Custom row render function
-const renderRow = (item: LedgerListItem, index: number) => (
-  <tr
-    key={index}
-    className="bg-white w-full text-[13px] text-left font-medium text-tableText h-[40px]"
-  >
-    <td className="py-1 px-4">{item?.id}</td>
-    <td className="py-1 px-4">{item?.acctName}</td>
-    <td className="py-1 px-4">{item?.parentID}</td>
-    <td className="py-1 px-4">{item?.branchID}</td>
-    <td className="py-1 px-4">{item?.ledgerClassID}</td>
-    <td className="py-1 px-4">{item?.ledgerSubClassID}</td>
-    <td className="py-1 px-4">{item?.accountClassificationID}</td>
-    <td className="py-1 px-4">{item?.currencyCode}</td>
-    <td className="py-1 px-4 align-middle">
-      <span
-        className={` ${
-          item?.status === false
-            ? "bg-[#FFDEDC] text-[#FF361B]"
-            : "text-[#14804A] bg-[#E1FCEF]"
-        }  rounded-sm flex justify-center items-center gap-2 h-[22px] w-[92px]`}
-      >
-        <SquareDotIcon />
-        {item?.status}
-      </span>
-    </td>
-  </tr>
-);
+const renderRow = (item: LedgerListItem, index: number) => {
+
+  return (
+    <tr
+      key={index}
+      className="bg-white w-full text-[13px] text-left font-medium text-tableText h-[40px]"
+    >
+      {/* <td className="py-1 px-4">{item?.id}</td> */}
+      <td className="py-1 px-4">{index+ 1}</td>
+      <td className="py-1 px-4">{item?.acctName}</td>
+      <td className="py-1 px-4 text-center">{item?.regionID}</td>
+      <td className="py-1 px-4 text-center">{item?.branchID}</td>
+      <td className="py-1 px-4 text-center">{item?.ledgerClassID}</td>
+      <td className="py-1 px-4 text-center">{item?.ledgerSubClassID}</td>
+      <td className="py-1 px-4 text-center">{item?.accountClassificationID}</td>
+      <td className="py-1 px-4">{item?.currency?.curr}</td>
+      <td className="py-1 px-4 align-middle">
+        <span
+          className={` ${
+            item?.status === false
+              ? "bg-[#FFDEDC] text-[#FF361B]"
+              : "text-[#14804A] bg-[#E1FCEF]"
+          }  rounded-sm flex justify-center items-center gap-2 h-[22px] w-[92px]`}
+        >
+          <SquareDotIcon />
+          {item?.status === false ? "false" : "true"}
+        </span>
+      </td>
+    </tr>
+  );
+};
 
 const LedgerListTable = ({ listData }: listType) => {
+  console.log("listData", listData);
+
   return (
     <div className="bg-[#E7EEFA]">
       <OperationsTableComp
@@ -57,7 +63,6 @@ const LedgerListTable = ({ listData }: listType) => {
         data={listData}
         renderRow={renderRow}
       />
-      <Pagination />
     </div>
   );
 };
